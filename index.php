f8cb3a7ff7a2362c88da174cfb6a63b8c245e0bb
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-19 09:12:29 -0600

    initial commit of index page. Test instantiate.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>snap-object</title>
+	</head>
+	<body>
+		<h1>Test Snap Object</h1>
+	</body>
+	<?php include("userprofile.php"); ?>
+	<?php
+
+	?>
+</html>
